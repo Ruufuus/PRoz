75c23646f2f339721ebc6fbe5e1fabb7a09329f7
VM <rufusikx3@gmail.com>
2020-06-06 21:13:07 +0200

    Add function broadcast_mission to class Employer

--- employer.cpp
+++ employer.cpp
@@ -7,4 +7,8 @@ class Employer {
         process_id = MPI_COMM_RANK;
     }
 
+    void broadcast_mission(){
+        if(DEBUG)printf("Wyslano informacje o misji z %d procesu do wszystkich procesow!\n",MPI_COMM_RANK);
+        MPI_Bcast(null, 0, MPI_INT, MPI_COMM_RANK, MISSION);
+    }
 };
