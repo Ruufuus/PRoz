b1889265d1bf91e2dc6399e930e015e817e8eee3
Rufus <rufusikx3@gmail.com>
2020-06-06 18:13:42 +0200

    Add struct specialist

---
+++ specialist.h
@@ -0,0 +1,9 @@
+struct specialist {
+	enum type { TAIL, BODY, HEAD };
+	int expert_needed;
+	int guild_table_count;
+	int initial_skeleton_count;
+	int lamport_clock_value;
+	int team_ids[3];
+	int mission_unassigned;
+};
