1b5eaa57e722888c6b4ed0f7b8957cf6dcee157b
VM <rufusikx3@gmail.com>
2020-06-06 22:41:00 +0200

    doesnt work

--- dragon_ressurection.cpp
+++ dragon_ressurection.cpp
@@ -1,10 +1,20 @@
 #include<iostream>
 #include "constants.h"
 #include "employer.cpp"
+#include "specialist_1.cpp"
 int main(int argc, char *argv[]){
 
     MPI_Init(&argc,&argv);
     Employer employer;
+    specialist sp;
+    sp.mission_unassigned = 0;
+    Specialist_1 specialist_1__(sp);
+    int process_id; 
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
+    if(process_id == 1)
     employer.process_lifetime();
+    else
+    specialist_1__.wait_for_mission();
+
     return 0;
 }
--- specialist_1.cpp
+++ specialist_1.cpp
@@ -6,7 +6,7 @@ class Specialist_1{
     public:
         Specialist_1(specialist data){
             MPI_Comm_rank(MPI_COMM_WORLD, &process_id);
-            this.data = data;
+            this->data = data;
         }
         void wait_for_mission(){
             bool is_mission = false;
@@ -15,13 +15,13 @@ class Specialist_1{
                 MPI_Gather(NULL, 0, MPI_BYTE, &message, 1, MPI_INT, process_id, MPI_COMM_WORLD);
                 if(DEBUG)printf("Otrzymano wiadomosc o tresci %d!\n",message);
                 if(message == MISSION){
-                    this.data.mission_unassigned+=1;
+                    this->data.mission_unassigned+=1;
                 }
                 else if(message == MTAK1){
-                    this.data.mission_unassigned-=1;
+                    this->data.mission_unassigned-=1;
                 }
             }
         }
 
     
-}
+};
