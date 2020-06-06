ec1f510dd81272974cb1b9ef22cb90cd81fce174
Rufus <rufusikx3@gmail.com>
2020-06-06 18:02:18 +0200

    define message tags

--- constants.h
+++ constants.h
@@ -1,6 +1,36 @@
 #ifndef CONSTANTS
 #define CONSTANTS
 
+#define MISSION 100
 
+#define MREQ1 101
+#define MREQ2 102
+#define MREQ3 103
+
+#define MACK1 111
+#define MACK2 112
+#define MACK3 123
+
+#define MTAK1 121
+#define MTAK2 122
+#define MTAK3 123
+
+#define S2IFREQ 132
+#define S3IFREQ 133
+
+#define S2REQ 142
+#define S3REQ 143
+
+#define TREADY 200
+
+#define TREQ 300
+
+#define TACK 310
+
+#define SKREQ 400
+
+#define SKACK 410
+
+#define RREADY 500
 
 #endif
