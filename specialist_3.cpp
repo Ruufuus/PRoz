ab652c41f9cde1456f5654f7d50f461022a24cbd
delrisu <emeryt360@gmail.com>
2020-06-07 20:28:53 +0200

    Add prepare_for_ressurection() in Specialist_3

--- specialist_3.cpp
+++ specialist_3.cpp
@@ -39,18 +39,19 @@ class Specialist_3: public Thread {
                     case MREQ3 :
                         if (this->data.lamport_clock_value < message_buffor[0] || 
                         (this->data.lamport_clock_value == message_buffor[0] && this->process_id < status.MPI_SOURCE)){
-                            this->data.lamport_clock_value += 1;
+                            this->data.lamport_clock_value++;
                             message = this->data.lamport_clock_value;
                             MPI_Send(&message, 1, MPI_INT, status.MPI_SOURCE, MACK3, MPI_COMM_WORLD);
                         }
                         break;
                     case S3REQ :
-                        this->data.mission_unassigned += 1;
+                        this->data.mission_unassigned++;
                         this->data.team_ids[0] = message_buffor[1];
                         this->data.team_ids[1] = message_buffor[2];
+                        is_S3REQ = true;
                         break;
                     case MTAK3 :
-                        this->data.mission_unassigned -= 1;
+                        this->data.mission_unassigned--;
                     default:
                         break;
                 }
@@ -60,11 +61,58 @@ class Specialist_3: public Thread {
         }
         void report_team_ready(){
             int team_mess[4];
-            team_mess[1] = this->data.lamport_clock_value + 1;
-            memcpy(&(team_mess[2]),this->data.team_ids, sizeof(int)*3);
+            team_mess[0] = this->data.lamport_clock_value + 1;
+            memcpy(&(team_mess[1]),this->data.team_ids, sizeof(int)*3);
 
-            for(int i = 1; i < 3; i++){
+            for(int i = 0; i < 2; i++){
                 MPI_Send(&team_mess, 4, MPI_INT, this->data.team_ids[i], TREADY, MPI_COMM_WORLD);
             }
+        }
+
+        void prepare_for_ressurection() {
+            this->data.lamport_clock_value++;
+            int message = this->data.lamport_clock_value;
+
+            for(int i = 0; i < 2; i++){
+                MPI_Send(&message, 1, MPI_INT, this->data.team_ids[i], TREADY, MPI_COMM_WORLD);
+            }
+
+            int mess_buf[4];
+            int rready_count = 0;
+            bool team_ready = false;
+
+            while(!team_ready){
+                MPI_Recv(&mess_buf, 4, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+                switch(status.MPI_TAG){
+                    case RREADY :
+                        this->data.lamport_clock_value = std::max(this->data.lamport_clock_value, mess_buf[0])+1;
+                        rready_count++;
+                        if(rready_count == 2){
+                            if(DEBUG)printf("[RESSURECT]\t%d\tZaczyna wskrzeszanie!\n",this->process_id);
+                            sleep(5);
+                            if(DEBUG)printf("[RESSURECT]\t%d\tKonczy wskrzeszanie!\n",this->process_id);
+                            team_ready = true;
+                        }
+                        break;
+                    case MREQ3 :
+                        this->data.lamport_clock_value = std::max(this->data.lamport_clock_value, mess_buf[0])+1;
+                        message = this->data.lamport_clock_value;
+                        MPI_Send(&message, 1, MPI_INT, status.MPI_SOURCE, MPI_COMM_WORLD);
+                        break;
+                    case S3REQ :
+                        this->data.lamport_clock_value = std::max(this->data.lamport_clock_value, mess_buf[0])+1;
+                        this->data.mission_unassigned++;
+                        break;
+                    case MTAK3 :
+                        this->data.lamport_clock_value = std::max(this->data.lamport_clock_value, mess_buf[0])+1;
+                        this->data.mission_unassigned--;
+                        break;
+                    case default:
+                        break;
+                }
+            }
+
+            
         }
 }
