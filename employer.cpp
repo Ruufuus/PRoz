3a2eb61e31b953a10991ba6a3a8498181a1b89e6
Rafał Wójcik <45013153+Ruufuus@users.noreply.github.com>
2020-06-14 20:43:10 +0200

    Update employer.cpp

--- employer.cpp
+++ employer.cpp
@@ -7,9 +7,10 @@ class Employer: public Thread{
 
     void broadcast_mission(){
         if(DEBUG)printf("[EMPLOYER_BM]\t%d\tWyslano MISSION!\n",process_id);
+        int msg = 0;
         for(int i = 0; i<process_count; i++){
             if(process_id == i) continue;
-            MPI_Send(NULL, 0, MPI_INT, i, MISSION ,MPI_COMM_WORLD);
+            MPI_Send(msg, 1, MPI_INT, i, MISSION ,MPI_COMM_WORLD);
         }
     }
 
